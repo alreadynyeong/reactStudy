942c07c6abf921600580d0414838d70e2412eb9d
alreadynyeong <kjw87130@naver.com>
2022-04-11 11:07:48 +0900

    리액트 초기세팅, 로그인&글쓰기 구현

---
+++ src/components/base/Header.js
@@ -0,0 +1,13 @@
+import styled from 'styled-componnets';
+
+const HeaderBlock = styled.div``;
+
+const  Header = () => {
+    return (
+        < HeaderBlock>
+
+        </ HeaderBlock>
+    );
+};
+
+export default  Header;
