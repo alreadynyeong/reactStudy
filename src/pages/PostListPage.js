e21742a26c4788d726de9d9916d8a824559e7dd5
alreadynyeong <kjw87130@naver.com>
2022-04-13 12:39:43 +0900

    #1 포스트 리스트 UI 구현

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,12 +1,13 @@
 // import Button from "../components/common/Button";
 // import Header from "../components/common/Header";
 import HeaderContainer from "../containers/common/HeaderContainer";
+import PostList from "../components/post/PostList";
 
 const PostListPage = () => {
     return (
         <>
             <HeaderContainer />
-            <div>안녕하세요.</div>
+            <PostList />
         </>
     );
 };
