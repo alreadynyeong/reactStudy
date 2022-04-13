494b458cfcb4b49160edfa0c680c5f1328fa3218
alreadynyeong <kjw87130@naver.com>
2022-04-13 14:09:20 +0900

    #1 pagenation 추가

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -3,12 +3,14 @@
 import HeaderContainer from "../containers/common/HeaderContainer";
 // import PostList from "../components/post/PostList";
 import PostListContainer from "../containers/posts/PostListContainer";
+import PaginationContainer from "../containers/posts/PaginationContainer";
 
 const PostListPage = () => {
     return (
         <>
             <HeaderContainer />
             <PostListContainer />
+            <PaginationContainer />
         </>
     );
 };
