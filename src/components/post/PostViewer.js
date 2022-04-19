13b0a0ef8e3f27c32ede6e96a4d041dc92f0d7ff
alreadynyeong <kjw87130@naver.com>
2022-04-19 18:22:17 +0900

    #2 PostActionButtons 컴포넌트 만들기

--- src/components/post/PostViewer.js
+++ src/components/post/PostViewer.js
@@ -47,7 +47,7 @@ const PostContent = styled.div`
     color: ${palette.gray[8]};
 `;
 
-const PostViewer = ({ post, error, loading }) => {
+const PostViewer = ({ post, error, loading, actionButtons }) => {
     if (error) {
         if (error.respnse && error.response.status === 404) {
             return <PostViewerBlock>존재하지 않는 포스트입니다.</PostViewerBlock>;
@@ -77,6 +77,7 @@ const PostViewer = ({ post, error, loading }) => {
                     ))}
                 </Tags>
             </PostHead>
+            {actionButtons}
             <PostContent dangerouslySetInnerHTML={{__html: body}} />
         </PostViewerBlock>
     );
